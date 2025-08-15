14733a86e5cf3b33c25a51118f85434f10b7cd9f
s-m-sharafat-hossain <sharafatcox60@gmail.com>
2025-08-15 19:12:21 +0600

    hi

--- Level 2 Identifire Keyword/Programming Basic/Rust/target/debug/deps/Leason1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\shara\Desktop\Programming Basic\Leason1\target\debug\deps\Leason1.exe: src\main.rs
-
-C:\Users\shara\Desktop\Programming Basic\Leason1\target\debug\deps\Leason1.d: src\main.rs
-
-src\main.rs:
--- Level 8 Arrays/Programming Basic/Rust/target/debug/deps/Leason1-64f5d78493181528.d
+++
@@ -1,5 +0,0 @@
-C:\Users\shara\Desktop\Programming Basic\Rust\target\debug\deps\libLeason1-64f5d78493181528.rmeta: src\main.rs
-
-C:\Users\shara\Desktop\Programming Basic\Rust\target\debug\deps\Leason1-64f5d78493181528.d: src\main.rs
-
-src\main.rs:
